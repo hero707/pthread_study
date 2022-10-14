99f260f07ed86a9710f8fea6de110a0a20505992
koyoungwoong <yuko@hallym.ac.kr>
2022-10-14 20:04:21 +0900

    2nd commit

--- exam0.c
+++ exam0.c
@@ -1,8 +1,11 @@
+/*
+  No synchronization scheme
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
- 
 
 int cnt;
 int flag[2] = {0,0};
@@ -11,9 +14,7 @@ int turn = 0;
 void *func0() {
 
     for (int i = 0; i < 100000; i++) {
-
         cnt++;
-
         printf("cnt1 :: %d\n", cnt);
     }
 
@@ -22,30 +23,23 @@ void *func0() {
 void *func1() {
 
     for (int i = 0; i < 100000; i++) {
-
         cnt++;
-
         printf("cnt2 :: %d\n", cnt);
     }
 }
 
 
 
-int main()
-
-{
+int main(){
 
      pthread_t thread1, thread2;
      int  iret1, iret2;
 
-
      iret1 = pthread_create( &thread1, NULL, func0, NULL);
      iret2 = pthread_create( &thread2, NULL, func1, NULL);
 
-
      pthread_join( thread1, NULL);
      pthread_join( thread2, NULL); 
-
 
      printf("Thread error: %d\n", 200000-cnt);
      exit(0);
